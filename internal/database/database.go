68bda32e32f8f1e9cf03fd753fe12d2239b387f4
Mrglxor <muhamadfarhan.inc@gmail.com>
2026-01-22 17:35:36 +0700

    fixx

--- internal/database/database.go
+++ internal/database/database.go
@@ -13,11 +13,6 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Jakarta",
 		os.Getenv("DB_HOST"),
